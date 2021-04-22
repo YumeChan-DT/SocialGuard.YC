fac1c9056a8ea6c47ddf40c67f0143810af7cdaa
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-04-22 12:05:04 +0200

    Update Trustlist result embed (Added account creation time)
     + Clean Usings
     + Bump Version to 2.1.3

--- Data/Components/AuthCredentials.cs
+++ Data/Components/AuthCredentials.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SocialGuard.YC.Data.Components
+﻿namespace SocialGuard.YC.Data.Components
 {
 	public record AuthCredentials(string Username, string Password);
 	public record AuthRegisterCredentials(string Username, string Email, string Password) : AuthCredentials(Username, Password);
