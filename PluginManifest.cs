c7966bb13c08cfb0be664d9db6768d5265426e82
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2021-06-08 01:37:52 +0200

    Draft BroadcastsListener
    Putting this on temporary hiatus, as YumeChan lacks the necessary core implementations to support this feature, as of yet.

--- PluginManifest.cs
+++ PluginManifest.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using SocialGuard.YC.Services.Security;
 using DSharpPlus;
+using System.Threading;
 
 namespace SocialGuard.YC
 {
@@ -19,6 +20,7 @@ namespace SocialGuard.YC
 		internal const string ApiConfigFileName = "api";
 		
 		private readonly ILogger<PluginManifest> logger;
+		private readonly BroadcastsListener broadcastsListener;
 		private readonly DiscordClient coreClient;
 
 		internal static string VersionString { get; private set; }
@@ -26,13 +28,13 @@ namespace SocialGuard.YC
 		public GuildTrafficHandler GuildTrafficHandler { get; }
 
 
-		public PluginManifest(DiscordClient client, ILogger<PluginManifest> manifestLogger, ILogger<GuildTrafficHandler> trafficLogger,
-			IConfigProvider<IApiConfig> apiConfig, IDatabaseProvider<PluginManifest> database, IHttpClientFactory httpClientFactory)
+		public PluginManifest(DiscordClient client, ILogger<PluginManifest> logger, IConfigProvider<IApiConfig> apiConfig, 
+			IDatabaseProvider<PluginManifest> database, IHttpClientFactory httpClientFactory, BroadcastsListener broadcastsListener)
 		{
 			VersionString ??= PluginVersion;
 			coreClient = client;
-			logger = manifestLogger;
-
+			this.logger = logger;
+			this.broadcastsListener = broadcastsListener;
 			TrustlistUserApiService apiService = new(httpClientFactory, apiConfig);
 			GuildTrafficHandler = new(trafficLogger, apiService, database);
 		}
@@ -40,6 +42,7 @@ namespace SocialGuard.YC
 		public override async Task LoadPlugin() 
 		{
 			coreClient.GuildMemberAdded += GuildTrafficHandler.OnMemberJoinedAsync;
+			await broadcastsListener.StartAsync(CancellationToken.None);
 
 			await base.LoadPlugin();
 
@@ -58,6 +61,7 @@ namespace SocialGuard.YC
 		public override IServiceCollection ConfigureServices(IServiceCollection services) => services
 			.AddSingleton<TrustlistUserApiService>()
 			.AddSingleton<AuthApiService>()
-			.AddSingleton<EncryptionService>();
+			.AddSingleton<EncryptionService>()
+			.AddHostedService<BroadcastsListener>();
 	}
 }
---
+++ Services/BroadcastsListener.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nodsoft.YumeChan.PluginBase.Tools;
+using SocialGuard.YC.Data.Models;
+using SocialGuard.YC.Data.Models.Config;
+
+namespace SocialGuard.YC.Services
+{
+	public class BroadcastsListener : IHostedService
+	{
+		private readonly ILogger<BroadcastsListener> logger;
+		private readonly DiscordClient discordClient;
+		private readonly HubConnection hubConnection;
+
+		public BroadcastsListener(ILogger<BroadcastsListener> logger, IConfigProvider<IApiConfig> configProvider, DiscordClient discordClient)
+		{
+			IApiConfig config = configProvider.InitConfig(PluginManifest.ApiConfigFileName).PopulateApiConfig();
+			this.logger = logger;
+			this.discordClient = discordClient;
+			hubConnection = new HubConnectionBuilder()
+				.WithUrl(config.ApiHost + "/hubs/trustlist")
+				.AddMessagePackProtocol()
+				.WithAutomaticReconnect()
+				.Build();
+
+			hubConnection.On<TrustlistEntry>("NotifyNewEntry", async (entry) =>
+				{
+					await TestPing();
+				});
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			// Loop is here to wait until the server is running
+			while (hubConnection.State is not HubConnectionState.Connected)
+			{
+				try
+				{
+					await hubConnection.StartAsync(cancellationToken);
+				}
+				catch
+				{
+					await Task.Delay(1000, cancellationToken);
+				}
+			}
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) => hubConnection.DisposeAsync().AsTask();
+
+		public async Task TestPing() => await (await discordClient.GetChannelAsync(815152787990118430)).SendMessageAsync("SignalR broadcast received.");
+	}
+}
